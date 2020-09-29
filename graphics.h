594e5d77a804639c67a2d6a2205822baf6ff8d02
Andrew Hett <andrewmhett@gmail.com>
2020-09-28 20:33:23 -0700

    Added operational buttons and simple main menu

--- graphics.h
+++ graphics.h
@@ -1,4 +1,7 @@
 #include <sys/ioctl.h>
+#include <thread>
+#include <vector>
+#include <unistd.h>
 using namespace std;
 
 int framerate=30;
@@ -6,6 +9,64 @@ int framerate=30;
 void update_screen(WINDOW* window){
 	wrefresh(window);
 	refresh();
+}
+
+void draw_string(int x, int y, string str, WINDOW* window){
+	mvprintw(y,x-str.length()/2,str.data());
+}
+
+class Button{
+	public:
+		int x1, x2, y1, y2;
+		string text;
+		WINDOW* window;
+		int color;
+		bool selected=false;
+		Button(int a, int b, int c, int d, string txt, WINDOW* win, int color_pair){
+			x1=a;
+			x2=b;
+			y1=c;
+			y2=d;
+			text=txt;
+			window=win;
+			color=color_pair;
+		}
+		void draw(){
+			mvhline(y1, x1, 0, x2-x1);
+			mvhline(y2, x1, 0, x2-x1);
+			mvvline(y1, x1, 0, y2-y1);
+			mvvline(y1, x2, 0, y2-y1);
+			mvaddch(y1, x1, ACS_ULCORNER);
+		 	mvaddch(y2, x1, ACS_LLCORNER);
+			mvaddch(y1, x2, ACS_URCORNER);
+		    	mvaddch(y2, x2, ACS_LRCORNER);
+			if (selected){
+				attron(COLOR_PAIR(color));
+			}
+			for (int x=x1+1;x<x2;x++){
+				for (int y=y1+1;y<y2;y++){
+					mvaddch(y,x,' ');
+				}
+			}
+			draw_string(x1+((x2-x1)/2),y1+((y2-y1)/2),text,window);
+			attroff(COLOR_PAIR(color));
+		}
+};
+
+void add_button(int x, int y, int width, int height, string text, WINDOW* window, int color_pair, vector<Button> &button_list){
+	Button new_button = Button(x-(width/2),x+(width/2),y-(height/2),y+(height/2),text,window,color_pair);
+	if (button_list.empty()){
+		new_button.selected=true;
+	}
+	new_button.draw();
+	button_list.push_back(new_button);
+}
+
+void draw_buttons(vector<Button> button_list){
+	for (vector<Button>::iterator it = button_list.begin(); it != button_list.end(); ++it){
+		Button button = *it;
+		button.draw();
+	}
 }
 
 WINDOW* new_window(){
@@ -21,9 +82,4 @@ WINDOW* new_window(){
 		new_win = newwin(size.ws_col/2, size.ws_col, starty/2, 0);
 	}
 	return new_win;
-}
-
-void draw_string(int x, int y, string str, WINDOW* window){
-	mvprintw(y,x-str.length()/2,str.data());
-	update_screen(window);
 }
--- main.cpp
+++ main.cpp
@@ -6,8 +6,17 @@
 #include "graphics.h"
 using namespace std;
 
+vector<Button> buttons;
+
+int button_index=0;
+
 int main(){
 	initscr();
+	start_color();
+	noecho();
+	init_pair(1,COLOR_WHITE,COLOR_BLACK);
+	init_pair(2,COLOR_WHITE,COLOR_BLUE);
+	attron(COLOR_PAIR(1));
 	cbreak();
 	refresh();
 	WINDOW* main_window=new_window();
@@ -16,8 +25,41 @@ int main(){
 	getmaxyx(main_window, height, width);
 	box(main_window,0,0);
 	attron(A_STANDOUT);
-	draw_string(width/2,height/4," Elite: Ascii ",main_window);
+	draw_string(width/2,height/6," Elite: Ascii ",main_window);
+	attroff(A_STANDOUT);
+	add_button(width/2,height/3,width*0.6,height/8,"Load Saved Game",main_window,2,buttons);
+	add_button(width/2,height/2,width*0.6,height/8,"New Game",main_window,2,buttons);
+	add_button(width/2,2*height/3,width*0.6,height/8,"Exit",main_window,2,buttons);
+	update_screen(main_window);
 	while (1){
+		switch(getch()){
+			//down arrow
+			case 66:
+				if (button_index+1<buttons.size()){
+					buttons[button_index].selected=false;
+					button_index++;
+					buttons[button_index].selected=true;
+					draw_buttons(buttons);
+					update_screen(main_window);
+				}
+				break;
+			//up arrow
+			case 65:
+				if (button_index-1>=0){
+					buttons[button_index].selected=false;
+					button_index--;
+					buttons[button_index].selected=true;
+					draw_buttons(buttons);
+					update_screen(main_window);
+				}
+				break;
+			//right arrow
+			case 67:
+				break;
+			//left arrow
+			case 68:
+				break;
+		}
 	}
 	endwin();
 	return 0;
